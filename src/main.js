6732da0da69e8005f8fa0c78a87eafbd1c6d6f45
如斯 <37902939+guanyuqi@users.noreply.github.com>
2020-12-22 11:37:16 +0800

    ssl配置完毕

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const app = require("./app");
 
 const fs = require("fs");
 const path = require("path");
+
 const keyPath = path.join(__dirname, "app/ssl/ssl.key");
 const pemPath = path.join(__dirname, "app/ssl/ssl.pem");
 const options = {
@@ -16,7 +17,7 @@ const options = {
 /* app.listen(config.APP_PORT, () => {
   console.log(`帝王${config.APP_PORT}号引擎!!发动~~~`);
 }); */
-console.log(app.callback);
+
 https.createServer(options, app.callback()).listen(config.APP_PORT, (err) => {
   if (err) {
     console.log("服务启动出错", err);
